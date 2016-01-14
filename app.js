c6a5e899225c6dc53242bffc413d4cd3b9ee8258
Felix Milea-Ciobanu <felixmilea@gmail.com>
2016-01-14 10:23:09 -0700

    peer to peer features

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 var express = require('express');
-var parser  = require('body-parser');
+var parser	= require('body-parser');
 
 var app = express();
 
@@ -12,28 +12,81 @@ app.set('json spaces', 4);
 app.use(parser.json());
 
 app.use(function(req, res, next) {
-  console.log(req.body);
-  next();
+	console.log(req.body);
+	next();
 });
 
-app.post('/location', function(req, res) {
-
-	MongoClient.connect("mongodb://localhost:27017/parco-proto", function(err, db) {
-		if (err) { return console.dir(err); }
-
-		var collection = db.collection('location');
-
-		collection.insert(req.body, function(err, result) {
+function mongoConnect() {
+	return new Promise(function(resolve, reject) {
+		MongoClient.connect('mongodb://localhost:27017/parco-proto', function(err, db) {
 			if (err) {
-				console.dir(err);
-				res.status(500).json({ message: 'DB error', date: new Date() });
+				reject(err);
 			} else {
-				res.status(201).json({ message: 'Success', date: new Date() });
+				resolve(db);
 			}
 		});
 
 	});
+}
 
+app.get('/list', function(req, res) {
+	mongoConnect()
+		.then(function(db) {
+			var collection = db.collection('location');
+
+			collection.aggregate([{ $group: { _id: '$uuid' } }], function(err, results) {
+				if (err) {
+					console.dir(err);
+					res.status(500).json({ message: 'db error', date: new Date() });
+				} else {
+					res.status(200).json({ message: 'success', data: results.map(function(r) {
+						return r._id;
+					}), date: new Date() });
+				}
+			});
+		})
+		.catch(console.dir);
+});
+
+
+app.get('/user/:uuid', function(req, res) {
+	mongoConnect()
+		.then(function(db) {
+			var collection = db.collection('location');
+
+			collection.find({ uuid: req.params.uuid })
+				.sort({ _id: -1 })
+				.limit(1)
+				.exec(function(err, result) {
+					if (err) {
+						console.dir(err);
+						res.status(500).json({ message: 'db error', date: new Date() });
+					} else if (!result.length) {
+						res.status(404).json({ message: 'not found', date: new Date() });
+					} else {
+						res.status(200).json({ message: 'success', data: result[1], date: new Date() });
+					}
+				});
+		})
+		.catch(console.dir);
+});
+
+
+app.post('/location', function(req, res) {
+	mongoConnect()
+		.then(function(db) {
+			var collection = db.collection('location');
+
+			collection.insert(req.body, function(err, result) {
+				if (err) {
+					console.dir(err);
+					res.status(500).json({ message: 'db error', date: new Date() });
+				} else {
+					res.status(201).json({ message: 'success', date: new Date() });
+				}
+			});
+		})
+		.catch(console.dir);
 });
 
 app.listen(1225);
